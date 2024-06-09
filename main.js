6638fa4ed52f0aaf40d3beeb60edd264d9d73409
Kelvin-Mutugi <kelvinmutugi925@gmail.com>
2024-06-10 01:44:19 +0300

    update

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-console.log('Hello World!');
+let startBtn = document.getElementById ('activate')
+let startTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let paused = true;
+let intervalid;
+let mins = 0;
+startBtn.addEventListener('click', () =>{
+  if (paused) {
+    paused = false;
+    startTime = Date.now() - elapsedTime;
+    intervalid = setInterval(updateTime, 75);
+  }
+}) 
+function updateTime() {
+  elapsedTime = Date.now() - startTime;
+  secs = Math.floor((elapsedTime / 1000) % 60)
+  mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+  hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60)
+  console.log(secs)
+  if (mins >= 1) {
+document.getElementById ('download').disabled = false
+  }
+
+}
 function slidedown() {
 let sess = document.getElementById ('activationsec')
 sess.style.maxHeight='100%'
 
-}
+} 
